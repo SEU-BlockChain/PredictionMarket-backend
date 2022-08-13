9f71b658944fba68b485e81f434a799e0ec801fd
realhuhu <2661467107@qq.com>
2022-08-13 18:34:08 +0800

    fix

--- backend/apps/common/views.py
+++ backend/apps/common/views.py
@@ -74,3 +74,23 @@ class ImageView(ViewSet):
         COS.put_obj(file, image)
 
         return APIResponse(response_code.SUCCESS_POST_ARTICLE_IMAGE, "成功", {"data": image})
+
+    @action(["POST"], False)
+    def issue(self, request):
+        file = request.data.get("file")
+        name = "".join(str(uuid4()).split("-"))
+        form = str(file).split(".")[-1]
+
+        if not isinstance(file, File):
+            return APIResponse(response_code.WRONG_FORM, "请上传图片")
+
+        if form.lower() not in ("jpg", "png", "bmp", "jpeg", "gif"):
+            return APIResponse(response_code.WRONG_FORM, "不支持的图片格式")
+
+        if file.size / (1024 * 1024) > 5:
+            return APIResponse(response_code.EXCEEDED_SIZE, "图片不能超过5M")
+
+        image = f"issue/{name}.{form}"
+        COS.put_obj(file, image)
+
+        return APIResponse(response_code.SUCCESS_POST_ISSUE_IMAGE, "成功", {"data": image})
