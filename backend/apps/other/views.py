d397eb1aa68490c7c8ca35ef36dac14bb13c5444
realhuhu <2661467107@qq.com>
2022-10-03 17:46:15 +0800

    collection

--- backend/apps/other/views.py
+++ backend/apps/other/views.py
@@ -7,4 +7,4 @@ from backend.libs.wraps.response import APIResponse
 class InfoView(ViewSet):
     @action(["GET"], False)
     def version(self, request):
-        return APIResponse(result={"version": [0, 1, 0, 4]})
+        return APIResponse(result={"version": [0, 1, 0, 5]})
